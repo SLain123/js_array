9d3217657dbe4a3007d09a29c399eab2ef5837a8
SLain.workPC <sl163@ya.ru>
2020-02-14 16:51:30 +0300

    Доделал метод map

--- map.js
+++ map.js
@@ -1,15 +1,19 @@
 let array = [1, 2, 3, 4, 5, 6];
 
-const funMap = (args) => {
+const funMap = (args, fun) => {
 
  let store = [];
  for (let i = 0; i < args.length; i++) {
-  store[i] = args[i] * args[i];
+  store[i] = fun(args[i]);
  }
  return store;
 }
 
 
-console.log(funMap(array));
+const multy = (a) => {
+ return a * a;
+}
 
-// let sqare = map(array, item => item*item);
+console.log(funMap(array, multy)); // Функция
+
+console.log(array.map(multy));
