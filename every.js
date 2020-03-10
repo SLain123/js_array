560f449b9f4cb83a10117c1598bece12001a4de5
SLain <sl163@ya.ru>
2020-03-10 14:50:29 +0300

    Функция эмитирующая метод every, работает и коммент

---
+++ every.js
@@ -0,0 +1,15 @@
+const every = (array, test) => {
+    let result = true;
+    for (let i = 0; i < array.length; i++) {
+        if (test(array[i]) == false) 
+        result = false;
+    }
+    return result;
+  }
+  
+console.log(every([1, 3, 5], n => n < 10));
+  // → true
+console.log(every([2, 4, 16], n => n < 10));
+  // → false
+console.log(every([], n => n < 10));
+  // → true
